332e2f5b272005271b6c64f5f3165ca172117cf5
truongcuto107 <dct107207@gmail.com>
2026-04-03 02:35:06 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -180,4 +180,4 @@ app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply))
 
-app.run_polling()
+app.run_polling(drop_pending_updates=True)
